c7a5b946022d8dbdb7eef82aaef2e40605885595
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-10-05 00:05:53 -0500

    completed list methods

--- beginners/day02_List.py
+++ beginners/day02_List.py
@@ -2,9 +2,7 @@
 
 # You can initialize a list as:
 
-arr = list()
-# or simply
-arr = []
+# arr = list() # or simply arr = []
 
 #Lists in Python are very versatile. You can add almost anything in a Python list.
 # In Python, you can create a list of any objects: strings, integers, or even lists. 
@@ -16,17 +14,17 @@ arr = [1,2,3]
 
 # 1.) append(x) Adds a single element x to the end of a list.
 arr.append(9)   
-print arr 
+print(arr)
 # prints [1, 2, 3, 9]
 
 # 2.) extend(L) Merges another list L to the end.
 arr.extend([10,11])
-print arr
+print(arr)
 # prints [1, 2, 3, 9, 10, 11]
 
 # 3.) insert(i,x) Inserts element x at position i.
 arr.insert(3,7)
-print arr
+print(arr)
 # prints [1, 2, 3, 7, 9, 10, 11]
 
 # 4.) remove(x) Removes the first occurrence of element x.
@@ -36,10 +34,25 @@ arr
 
 # 5.) pop() Removes the last element of a list. If an argument is passed, that index item is popped out.
 temp = arr.pop()
-print temp 
+print(temp)
 # prints 11
 
 # 6.) index(x) Returns the first index of a value in the list. Throws an error if it's not found.
 temp = arr.index(3)
-print temp
+print(temp)
 # prints 2
+
+#7.) count(x) Counts the number of occurrences of an element x.
+temp = arr.count(1)
+print(temp)
+# prints 1
+
+# 8.) sort() Sorts the list.
+arr.sort()
+print(arr)
+# [1, 2, 3, 7, 9]
+
+# 9.) reverse() Reverses the list.
+arr.reverse()
+print(arr)
+# [9, 7, 3, 2, 1]
