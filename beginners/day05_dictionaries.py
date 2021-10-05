88602eba0c2831b83cf196205604a2fabffd7853
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-10-05 01:25:00 -0500

    Update day05_dictionaries.py

--- beginners/day05_dictionaries.py
+++ beginners/day05_dictionaries.py
@@ -23,3 +23,18 @@ print(a_dict)
 # prints {'two': 2}
 
 # Note: A dictionary is unordered. So, only use the keys to navigate through the dictionary.
+
+
+# -----------------------------------------
+# Finding the percentage
+# -----------------------------------------
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    query_scores = student_marks[query_name]
+    print("{0:.2f}".format(sum(query_scores)/(len(query_scores))))
