7e265978d4371839a33ecd93f7b83b09b62bac35
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-10-10 23:45:25 -0500

    Update day03_Tuples.py

--- beginners/day03_Tuples.py
+++ beginners/day03_Tuples.py
@@ -1,1 +1,10 @@
-print('day3')
+'''
+Tuples are data structures that look a lot like lists. Unlike lists, tuples are immutable (meaning that they cannot be modified once created). This restricts their use because we cannot add, remove, or assign values; however, it gives us an advantage in space and time complexities.
+
+A common tuple use is the swapping of  numbers:
+
+a, b = b, a
+Here  is a tuple, and it assigns itself the values of .
+
+Another awesome use of tuples is as keys in a dictionary. In other words, tuples are hashable.
+'''
