98f205feb24380efe2153e8520d199d99c4e51b1
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-10-19 14:14:10 -0500

    Create day04_Strings.py

---
+++ beginners/day04_Strings.py
@@ -0,0 +1,63 @@
+# Python Program for
+# Creation of String
+
+# Creating a String
+# with single Quotes
+String1 = 'Welcome to the Geeks World'
+print("String with the use of Single Quotes: ")
+print(String1)
+
+# Creating a String
+# with double Quotes
+String1 = "I'm a Geek"
+print("\nString with the use of Double Quotes: ")
+print(String1)
+
+# Creating a String
+# with triple Quotes
+String1 = '''I'm a Geek and I live in a world of "Geeks"'''
+print("\nString with the use of Triple Quotes: ")
+print(String1)
+
+# Creating String with triple
+# Quotes allows multiple lines
+String1 = '''Geeks
+			For
+			Life'''
+print("\nCreating a multiline String: ")
+print(String1)
+
+
+# Python Program to Access
+# characters of String
+
+String1 = "GeeksForGeeks"
+print("Initial String: ")
+print(String1)
+
+# Printing First character
+print("\nFirst character of String is: ")
+print(String1[0])
+
+# Printing Last character
+print("\nLast character of String is: ")
+print(String1[-1])
+
+
+# Python Program for
+# Formatting of Strings
+
+# Default order
+String1 = "{} {} {}".format('Geeks', 'For', 'Life')
+print("Print String in default order: ")
+print(String1)
+
+# Positional Formatting
+String1 = "{1} {0} {2}".format('Geeks', 'For', 'Life')
+print("\nPrint String in Positional order: ")
+print(String1)
+
+# Keyword Formatting
+String1 = "{l} {f} {g}".format(g = 'Geeks', f = 'For', l = 'Life')
+print("\nPrint String in order of Keywords: ")
+print(String1)
