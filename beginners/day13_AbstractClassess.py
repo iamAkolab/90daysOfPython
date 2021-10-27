2c5acd537364126522789f3d2bb263e72d6af3d0
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-10-27 00:47:13 -0500

    Update day13_AbstractClassess.py

--- beginners/day13_AbstractClassess.py
+++ beginners/day13_AbstractClassess.py
@@ -59,3 +59,34 @@
 # Title: The Alchemist
 # Author: Paulo Coelho
 # Price: 248
+
+# ========================
+#         Solution
+# ========================
+
+from abc import ABCMeta, abstractmethod
+class Book(object, metaclass=ABCMeta):
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author   
+    @abstractmethod
+    def display(): pass
+
+#Write MyBook class
+class MyBook(Book):
+    def __init__(self,title,author,price):
+        self.title=title
+        self.author=author
+        self.price = price
+        
+    def display(self):
+       print("Title:" , title,end='\n')
+       print("Author:", author,end='\n')
+       print("Price:", price, end='\n')
+
+title=input()
+author=input()
+price=int(input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
+
