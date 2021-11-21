3193cfd3ac8cea24ac3a2c646e8d4aa92f57c954
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-11-20 21:37:31 -0600

    Update day04_Strings.py

--- beginners/day04_Strings.py
+++ beginners/day04_Strings.py
@@ -1,14 +1,11 @@
-# Python Program for
-# Creation of String
+# Python Program for Creation of String
 
-# Creating a String
-# with single Quotes
+# Creating a String with single Quotes
 String1 = 'Welcome to the Geeks World'
 print("String with the use of Single Quotes: ")
 print(String1)
 
-# Creating a String
-# with double Quotes
+# Creating a String with double Quotes
 String1 = "I'm a Geek"
 print("\nString with the use of Double Quotes: ")
 print(String1)
