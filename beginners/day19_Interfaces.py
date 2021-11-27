6df0f594bf82aa4d19b30cf0336501a87ecdea76
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-11-27 11:10:07 -0600

    Update day19_Interfaces.py

--- beginners/day19_Interfaces.py
+++ beginners/day19_Interfaces.py
@@ -28,10 +28,28 @@ I implemented: AdvancedArithmetic
 12
 Explanation
 
-The integer 6 is evenly divisible by 1, 2, 3, and 6. Our divisorSum method should return the sum of these numbers, which is 1 + 2 + 3 + 6 = 12. The Solution class then prints I implemented: AdvancedArithmetic on the first line, followed by the sum returned by divisorSum (which is 12) on the second line
+The integer 6 is evenly divisible by 1, 2, 3, and 6. Our divisorSum method should return the sum of these numbers, which is 1 + 2 + 3 + 6 = 12. 
+The Solution class then prints I implemented: AdvancedArithmetic on the first line, followed by the sum returned by divisorSum (which is 12) on the second line
 
 """
 # ========================== #
 #         Solution           #
 # ========================== #
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
 
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        factors = 0
+        for i in range(1,n+1):
+            if(n % i == 0):
+                factors += i
+        return factors
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
