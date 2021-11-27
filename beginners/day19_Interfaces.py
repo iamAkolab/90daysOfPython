303e596aeab721cc40b0f4a9a313651844f6b786
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-11-26 23:29:52 -0600

    Create day19_Interfaces.py

---
+++ beginners/day19_Interfaces.py
@@ -0,0 +1,8 @@
+# Task
+"""
+The AdvancedArithmetic interface and the method declaration for the abstract divisorSum(n) method are provided for you in the editor below.
+
+Complete the implementation of Calculator class, which implements the AdvancedArithmetic interface. 
+The implementation for the divisorSum(n) method must return the sum of all divisors of .
+
+"""
